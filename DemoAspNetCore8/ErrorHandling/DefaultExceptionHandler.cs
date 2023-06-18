b57698e569a20a42490793a37efd0fcee77684e6
Anthony Giretti <anthony.giretti@gmail.com>
2023-06-18 09:05:58 -0400

    add aspnetcore 8 demo

---
+++ DemoAspNetCore8/ErrorHandling/DefaultExceptionHandler.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Diagnostics;
+using Microsoft.AspNetCore.Mvc;
+using System.Net;
+
+namespace DemoAspNetCore8.ErrorHandling;
+
+/*
+public class DefaultExceptionHandler : IExceptionHandler
+{
+    private readonly ILogger<DefaultExceptionHandler> _logger;
+    public DefaultExceptionHandler(ILogger<DefaultExceptionHandler> logger)
+    {
+        _logger = logger;
+    }
+
+    public async ValueTask<bool> TryHandleAsync(HttpContext httpContext, Exception exception, CancellationToken cancellationToken)
+    {
+        _logger.LogError(exception, "An unexpected error occurred");
+
+        await httpContext.Response.WriteAsJsonAsync(new ProblemDetails
+        {
+            Status = (int)HttpStatusCode.InternalServerError,
+            Type = exception.GetType().Name,
+            Title = "An unexpected error occurred",
+            Detail = exception.Message,
+            Instance = $"{httpContext.Request.Method} {httpContext.Request.Path}"
+        });
+
+        return true;
+    }
+}
+*/
---
+++ DemoDotnet8/Program.cs
@@ -0,0 +1,27 @@
+﻿var product1 = new Product(); // Gives: Name = "Default", CategoryId = -1
+
+var product2 = new Product("Basic"); // Gives: Name = "Basic", CategoryId = 0
+
+var product3 = new Product("My product", 1); // Gives: Name = "Default", CategoryId = -1
+
+Console.ReadLine();
+
+public class Product(string name, int categoryId)
+{
+    public Product() : this("Default", -1) { ProductDescription = "InvalidProductId"; }
+    public Product(string name) : this(name, 0) { }
+
+    public string Name => name;
+    public int CategoryId => categoryId;
+
+    public string ProductDescription { get; private set; }
+}
+
+public record struct Product2(string Name)
+{
+    public Product2() : this("Disk") { CategoryId = int.MinValue; } // Uses internally the primary constructor
+
+    public Product2(int categoryId) : this("Disk") { CategoryId = categoryId; } // Uses internally the primary constructor
+
+    public int CategoryId { get; set; } = int.MinValue;
+}
